a9366347cf4de0c8d5b3c427fa2b86a276f45780
VannucciS <50140951+VannucciS@users.noreply.github.com>
2021-03-26 15:09:56 -0300

    Create 9.6.2.py

---
+++ 9.6.2.py
@@ -0,0 +1,4 @@
+'''
+Write a function replace that consumes a list, an item old, and an item new and mutates the list by replacing old by new.
+If the item old is not in the list, the list should not change. You can assume that all items in the input are distinct (that is, none are the same as any others).
+'''
