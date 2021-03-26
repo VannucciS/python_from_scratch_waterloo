6f0712c2fd2ab5350980c1c8eed10a84e71913df
VannucciS <50140951+VannucciS@users.noreply.github.com>
2021-03-26 15:10:28 -0300

    Update and rename 9.6.2.py to exercise_9.6.2.py

--- exercise_9.6.2.py
+++ exercise_9.6.2.py
@@ -2,3 +2,9 @@
 Write a function replace that consumes a list, an item old, and an item new and mutates the list by replacing old by new.
 If the item old is not in the list, the list should not change. You can assume that all items in the input are distinct (that is, none are the same as any others).
 '''
+def replace(lista,old,new):
+    lista = [new if i in old else i for i in lista]
+    return lista
+
+
+print(replace(["a", "b"], 'a','d'))
