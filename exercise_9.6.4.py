d50a011ad8c0bc3801d77b2e1be116a90d689900
VannucciS <50140951+VannucciS@users.noreply.github.com>
2021-03-27 10:27:11 -0300

    Update exercise_9.6.4.py

--- exercise_9.6.4.py
+++ exercise_9.6.4.py
@@ -1,3 +1,7 @@
 '''
 Write a function max_diff that consumes a nonempty list of numbers and produces the maximum difference between any two elements in the list. Do not sort the list.
 '''
+def max_diff(ls):
+    return max(ls)-min(ls)
+
+print(max_diff([1]))
