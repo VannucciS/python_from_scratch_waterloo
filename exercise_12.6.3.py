933b13342335a2d0314d3478c7052ab6e7fe7c49
VannucciS <50140951+VannucciS@users.noreply.github.com>
2021-05-05 22:24:56 -0300

    Update exercise_12.6.3.py

--- exercise_12.6.3.py
+++ exercise_12.6.3.py
@@ -13,6 +13,16 @@ Make sure your function works when the number of objects containing the string i
 Make sure your function works when the input list is of length 0.
 '''
 
+'''
+Instructions
+Now create a function (not a method) all_contain that consumes a list of Meal objects and a string allergen. 
+Your function should produce a list of the names of all objects in the list that contain the string in their allergen list.
+
+Hints:
+Make sure that your function definition is not part of the class definition.
+Keep track of what is a list and what is an object.
+'''
+
 class Meal:
     """Meal name, cost, and list of allergens
 
@@ -37,12 +47,9 @@ class Meal:
         else:
             return False
             
-def all_contain(lst,item):
-    allergy = set()
-    if len(lst) == 0 or len(item)==0:
-        return 'Empty'
-    for i in lst:
-        for e in item:
-            if i == e:
-                allergy.add(i)
-    return allergy
+def all_contain(meals, allergen):
+    found = list()
+    for meal in meals:
+        if allergen in meal.allergens:
+            found.append(meal.name)
+    return found
